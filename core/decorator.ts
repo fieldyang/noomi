7e4eef55465cf5f2bed669a76305bba3558282bc
fieldyang <fieldyang@163.com>
2019-11-10 23:10:45 +0800

    增加装饰器注释

--- core/decorator.ts
+++ core/decorator.ts
@@ -10,10 +10,10 @@ import { Util } from './util';
 
 
 /**
- * instance装饰器（添加到实例工厂）
- * @param cfg
- *              name:       实例名
- *              singleton:  是否单例
+ * instance装饰器，添加实例到实例工厂，装饰类
+ * @param cfg:object
+ *          name:string     实例名，必填
+ *          singleton:bool  是否单例，默认false
  */
 function Instance(cfg){
     return (target) =>{
@@ -29,8 +29,8 @@ function Instance(cfg){
 }
 
 /**
- * IoC注入装饰器
- * @param instanceName  实例名
+ * IoC注入装饰器，装饰属性
+ * @param instanceName:string  实例名，必填
  */
 function Inject(instanceName:string){
     return (target:any,propertyName:string)=>{
@@ -39,8 +39,10 @@ function Inject(instanceName:string){
 }
 
 /**
- * route config 路由类
- * @param cfg 
+ * 路由类装饰器，装饰类
+ * @param cfg:object
+ *          namespace:string    命名空间，namespace+该类下的所有方法对应的路由路径=路由完整路径，可选
+ *          path:string         路径，路由路径为 namespace+path+方法名，设置时，对所有方法有效，可选
  */
 function Router(cfg?:any){
     return (target)=>{
@@ -71,8 +73,17 @@ function Router(cfg?:any){
 }
 
 /**
- * 路由方法
- * @param cfg 
+ * 路由装饰器，装饰方法
+ * @param cfg:object
+ *          path:string                 路由路径，必填
+ *          results:Array<object>       结果数组，可选，如果results未设置或数组长度为0，则按json处理，单个结果元素包含:
+ *              value:any               方法返回结果，如果return值与value相同，则使用该结果
+ *              type:string             结果类型，可选值:redirect(重定向),chain(路由链),void(空),json，默认为json
+ *              url:string,             该方法对应的路由路径，完整路径=namespace+url，以/开头，
+ *                                      如Router装饰器设置的namespace为/user,url为/getinfo,则访问路由为/user/getinfo
+ *                                      type为redirect或chain时必须设置
+ *              params:Array<string>    参数名数组，当url为路由时，params中存放当前路由所处实例的属性名，将属性名和其对应值加入url指向路由的输入参数
+ *                                      如params:['p1','p2']，则传入url路由时的参数为{p1:v1,p2:v2}和路由自带的参数，可选
  */
 function Route(cfg:any){
     return (target:any,propertyName:string)=>{
@@ -80,13 +91,12 @@ function Route(cfg:any){
             let ns = target.__routeconfig?target.__routeconfig.namespace:'';
             RouteFactory.addRoute(ns + cfg.path,target.__instanceName,propertyName,cfg.results);
         });
-        
     }
 }
 /**
- * web过滤器
- * @param pattern   过滤正则表达式串，可以为数组
- * @param order     优先级，默认1000
+ * web过滤器，装饰方法
+ * @param pattern:string|Array<string>  过滤表达式串或数组，支持通配符，默认为/*，过滤所有路由
+ * @param order:number                  优先级，值越小优先级越高，默认10000，可选
  */
 function WebFilter(pattern?:any,order?:number){
     return function(target:any,name:string){
@@ -100,15 +110,20 @@ function WebFilter(pattern?:any,order?:number){
 }
 
 /**
- * 切面装饰器
+ * 切面装饰器，装饰类
  */
-function Aspect(target:any){
-    target.isAspect = true;
-    target.prototype.isAspect = true;
+function Aspect(){
+    return (target)=>{
+        target.prototype.__isAspect = true;
+    }
 }
 
 /**
- * 切点装饰器
+ * 切点装饰器，切点名为方法名+()，装饰方法
+ * @param expressions:string|Array<string> 切点需要拦截的表达式串或数组，支持通配符*，
+ *                                         拦截对象为instanceName.methodName，
+ *                                         如user*.*拦截实例名为user开头的实例下的所有方法，
+ *                                         userX.m1拦截实例名为userX的m1方法
  */
 function Pointcut(expressions:any){
     return (target:any,name:string)=>{
@@ -117,8 +132,8 @@ function Pointcut(expressions:any){
 }
 
 /**
- * 通知装饰器 before
- * @param pointcutId    切点id
+ * 通知装饰器 before，装饰方法
+ * @param pointcutId:string    切点id
  */
 function Before(pointcutId:string){
     return (target:any,name:string,desc:any)=>{
@@ -132,8 +147,8 @@ function Before(pointcutId:string){
 }
 
 /**
- * 通知装饰器 after
- * @param pointcutId    切点id
+ * 通知装饰器 after，装饰方法
+ * @param pointcutId:string    切点id
  */
 function After(pointcutId:string){
     return (target:any,name:string,desc:any)=>{
@@ -147,8 +162,8 @@ function After(pointcutId:string){
 }
 
 /**
- * 通知装饰器 around
- * @param pointcutId    切点id
+ * 通知装饰器 around，装饰方法
+ * @param pointcutId:string    切点id
  */
 function Around(pointcutId:string){
     return (target:any,name:string,desc:any)=>{
@@ -161,8 +176,8 @@ function Around(pointcutId:string){
     }
 }
 /**
- * 通知装饰器 after return
- * @param pointcutId    切点id
+ * 通知装饰器 after-return，装饰方法
+ * @param pointcutId:string   切点id
  */
 function AfterReturn(pointcutId:string){
     return (target:any,name:string,desc:any)=>{
@@ -176,7 +191,7 @@ function AfterReturn(pointcutId:string){
 }
 
 /**
- * 通知装饰器 after throw
+ * 通知装饰器 after-throw，装饰方法
  * @param pointcutId    切点id
  */
 function AfterThrow(pointcutId:string){
@@ -191,10 +206,10 @@ function AfterThrow(pointcutId:string){
 }
 
 /**
- * 事务类装饰器
+ * 事务类装饰器，装饰类
  * 该装饰器必须放在Instance装饰器之前使用
  * 把符合条件的方法装饰为事务方法
- * @param methodReg 数组或字符串，方法名表达式，可以使用*通配符，默认为*，表示所有方法
+ * @param methodReg 数组或字符串，方法名表达式，可以使用*通配符，默认为*，表示该实例的所有方法都为事务方法
  */
 function Transactioner(methodReg?:any){
     return (target)=>{
@@ -222,7 +237,7 @@ function Transactioner(methodReg?:any){
     }
 }
 /**
- * 事务装饰器
+ * 事务装饰器，装饰方法
  */ 
 function Transaction(){
     return (target:any,name:string,desc:any)=>{
